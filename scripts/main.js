35499d4fd92345c556c66e16a809e4e1cb8fa9b9
Boti <kissiki99@gmail.com>
2020-09-23 17:57:40 +0200

    main js splited

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-import {printer} from "./game.js";
+import { gameEvents } from "./game.js";
 import { images, descriptions } from "./readImg.js";
 
 
@@ -30,61 +30,4 @@ randomNumbers.forEach(num => {
     rightCardContainer.append(descriptions[num])
 });
 
-const cards = document.getElementsByClassName("cards");
-
-for(const card of cards) {
-    card.addEventListener('dragover', dragOver);
-    card.addEventListener('dragenter', dragEnter);
-    card.addEventListener('dragleave', dragLeave);
-    card.addEventListener('dragstart', () => card.classList.add('dragging'));
-    card.addEventListener('dragend', () => card.classList.remove('dragging'));
-    card.addEventListener('drop', dragDrop);
-}
-
-function dragOver(e) {
-    e.preventDefault();
-}
-
-function dragEnter(e) {
-    e.preventDefault();
-    this.classList.add("hovered");
-}
-
-function dragLeave() {
-    this.classList.remove("hovered");
-}
-
-let pairCounter = 0;
-
-function dragDrop(e) {
-    e.preventDefault();
-    this.classList.remove("hovered");
-    const draggable = document.getElementsByClassName("dragging");
-    if (draggable[0].getAttribute('number') === this.getAttribute('number') && draggable[0] !== this) {
-        const pairsContainer = document.getElementById('pair-container');
-        const newDiv = document.createElement('div');
-        newDiv.classList.add('pairs');
-        if (draggable[0].getAttribute('imgNum') == 1) {
-            this.classList.add('first-pair-card');
-            this.classList.remove('cards');
-            newDiv.append(this);
-            draggable[0].classList.add('second-pair-card');
-            draggable[0].classList.remove('cards');
-            newDiv.append(draggable[0]);
-        } else {
-            draggable[0].classList.add('first-pair-card');
-            draggable[0].classList.remove('cards');
-            newDiv.append(draggable[0]);
-            this.classList.add('second-pair-card');
-            this.classList.remove('cards');
-            newDiv.append(this);
-        }
-        
-        pairsContainer.append(newDiv);
-        pairCounter++;
-        if (pairCounter >= 8) {
-            document.getElementById('hidden').style.display = "block";
-            document.getElementById('middle-line').style.display = "none";
-        }
-    }
-}
+gameEvents();
