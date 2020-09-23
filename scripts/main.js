7977902202da2a3c9cd1e43cd64ae95d05597271
Boti <kissiki99@gmail.com>
2020-09-23 13:10:50 +0200

    basic logic completed

--- scripts/main.js
+++ scripts/main.js
@@ -45,7 +45,9 @@ for(const card of cards) {
     card.addEventListener('dragover', dragOver);
     card.addEventListener('dragenter', dragEnter);
     card.addEventListener('dragleave', dragLeave);
-    card.addEventListener('drop', dragDrap);
+    card.addEventListener('dragstart', () => card.classList.add('dragging'));
+    card.addEventListener('dragend', () => card.classList.remove('dragging'));
+    card.addEventListener('drop', dragDrop);
 }
 
 function dragOver(e) {
@@ -61,7 +63,22 @@ function dragLeave() {
     this.classList.remove("hovered");
 }
 
-function dragDrap(e) {
+function dragDrop(e) {
     e.preventDefault();
     this.classList.remove("hovered");
+    const draggable = document.getElementsByClassName("dragging");
+    console.log(draggable[0].getAttribute('number'));
+    console.log(this.getAttribute('number'));
+    if (draggable[0].getAttribute('number') === this.getAttribute('number')) {
+        const pairsContainer = document.getElementById('pair-container');
+        const newDiv = document.createElement('div');
+        newDiv.classList.add('pairs');
+        this.classList.add('first-pair-card');
+        this.classList.remove('cards');
+        newDiv.append(this);
+        draggable[0].classList.add('second-pair-card');
+        draggable[0].classList.remove('cards');
+        newDiv.append(draggable[0]);
+        pairsContainer.append(newDiv);
+    }
 }
