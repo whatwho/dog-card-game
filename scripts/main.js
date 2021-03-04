2ab0139a610c307e2f3b8d8c7a72195b820096c0
CocoBloom <virag.baranyai2@gmail.com>
2021-03-04 20:36:15 +0100

    implement init(), import getPlayTime

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,46 @@
 import { gameEvents } from "./game.js";
 import { images, descriptions } from "./readImg.js";
+import { getPlayTime } from "./time.js";
 
-
-const leftCardContainer = document.getElementById("left-cards-conatiner");
-const rightCardContainer = document.getElementById("right-cards-conatiner");
-
-document.getElementById("restart").addEventListener("click", () => window.location.reload());
-
-let randomNumbers = [];
-
-for (let index = 0; index < 8; index++) {
-    let currentNum = Math.floor(Math.random() * 20);
-    while (randomNumbers.includes(currentNum)) {
-        currentNum = Math.floor(Math.random() * 20);
-    }
-    randomNumbers.push(currentNum);
-    images[currentNum].setAttribute("number", currentNum);
-    images[currentNum].setAttribute("imgNum", 1);
-    images[currentNum].classList.add("cards");
-    leftCardContainer.append(images[currentNum]);
+window.onload = function () {
+    init();
 }
 
-randomNumbers = randomNumbers.sort(() => Math.random() - 0.5)
+function init() {
+    document.getElementById("restart").addEventListener("click", () => window.location.reload());
+    buildGame();
+    gameEvents();
+    getPlayTime.start_time();
+}
 
-randomNumbers.forEach(num => {
-    descriptions[num].setAttribute("number", num);
-    descriptions[num].setAttribute("imgNum", 0);
-    descriptions[num].classList.add("cards");
-    rightCardContainer.append(descriptions[num])
-});
+function buildGame() {
+    const leftCardContainer = document.getElementById("left-cards-container");
+    const rightCardContainer = document.getElementById("right-cards-container");
 
-gameEvents();
+    let randomNumbers = [];
+
+    for (let index = 0; index < 8; index++) {
+        let currentNum = Math.floor(Math.random() * 20);
+        while (randomNumbers.includes(currentNum)) {
+            currentNum = Math.floor(Math.random() * 20);
+        }
+        randomNumbers.push(currentNum);
+        images[currentNum].setAttribute("number", currentNum);
+        images[currentNum].setAttribute("imgNum", 1);
+        images[currentNum].classList.add("cards");
+        leftCardContainer.append(images[currentNum]);
+    }
+
+    randomNumbers = randomNumbers.sort(() => Math.random() - 0.5)
+
+    randomNumbers.forEach(num => {
+        descriptions[num].setAttribute("number", num);
+        descriptions[num].setAttribute("imgNum", 0);
+        descriptions[num].classList.add("cards");
+        rightCardContainer.append(descriptions[num])
+    });
+}
+
+
+
+
