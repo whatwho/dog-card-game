20e8ea88e5669d758ed3be14770a5a68582334d6
CocoBloom <virag.baranyai2@gmail.com>
2021-03-04 20:37:16 +0100

    impor getPlayTime, is_win(), won_screen()

--- scripts/game.js
+++ scripts/game.js
@@ -1,4 +1,7 @@
+import { getPlayTime } from "./time.js";
+
 export function gameEvents() {
+
     const cards = document.getElementsByClassName("cards");
 
     for(const card of cards) {
@@ -48,14 +51,26 @@ export function gameEvents() {
                 this.classList.remove('cards');
                 newDiv.append(this);
             }
-            
             pairsContainer.append(newDiv);
             pairCounter++;
-            if (pairCounter >= 8) {
-                document.getElementById('hidden').style.display = "block";
-                document.getElementById('middle-line').style.display = "none";
-                document.getElementById('restart').style.top = "40%"
+
+            if (is_win(pairCounter)) {
+                getPlayTime.stop_timer();
+                won_screen();
             }
+        } else {
+            getPlayTime.increase_timer();
         }
+    }
+
+    function is_win(pairCounter) {
+        return pairCounter >= 8;
+    }
+    function won_screen() {
+        let temp_time = getPlayTime.getTime();
+        document.getElementById('hidden').innerText = "Megtaláltad az összes párt!:)\nJátékidő: " + temp_time['minute'] + ":" + temp_time['second'];
+        document.getElementById('hidden').style.display = "block";
+        document.getElementById('middle-line').style.display = "none";
+        document.getElementById('restart').style.top = "40%"
     }
 }
